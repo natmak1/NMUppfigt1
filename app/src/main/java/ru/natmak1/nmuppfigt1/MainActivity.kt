297cf83d1c734eddc88872e9596e35fe1da9c754
natmak1 <natmak1@yandex.ru>
2022-01-25 17:51:27 +0100

    start

---
+++ app/src/main/java/ru/natmak1/nmuppfigt1/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.natmak1.nmuppfigt1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
